eecf800e2517bcf4443f1ea2a6a056a02f52f51f
Lyagushka <rayz.sir@gmail.com>
2023-06-18 21:21:01 +0400

    fixes

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ function parseKeys() {
   if (data === '') {
     throw Error('no keys');
   }
-  const keys = data.split('\n');
+  const keys = data.replace(/\\r/g, '').split('\n');
   return keys;
 }
 
 async function fuck(min, max, maxTxs) {
   const keys = parseKeys();
-  let wallets = keys.map(key => new ethers.Wallet(key.replace('\r', '').startsWith('0x') ? key : '0x' + key, provider));
+  let wallets = keys.map(key => new ethers.Wallet(key.startsWith('0x') ? key : '0x' + key, provider));
   let walletData = {};
   for (let i = 0; i < wallets.length; i++) {
     walletData[wallets[i].address] = {
